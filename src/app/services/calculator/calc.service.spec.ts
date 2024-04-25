0db10dfe539995ff9e04ccec98d2f18d2e6aaa36
supun delpagoda <supun@savinda.lk>
2024-04-25 11:00:42 +0530

    Add TestBed

--- src/app/services/calculator/calc.service.spec.ts
+++ src/app/services/calculator/calc.service.spec.ts
@@ -1,15 +1,34 @@
+import { TestBed } from "@angular/core/testing"
 import { SharedService } from "../shared.service"
 import { CalcService } from "./calc.service"
 
 describe("CalcService", () => {
 
-  let shared : SharedService
+  let shared: SharedService
   let calc: CalcService
 
-  beforeEach(()=>{
+  beforeEach(() => {
     console.log("Before each called");
-    shared = new SharedService
-    calc = new CalcService(shared)
+    // shared = new SharedService // here we have manually created instances without dependancy injection
+    // calc = new CalcService(shared) // to awoid that we can use anguar testbed utility
+
+    // testbed is used to provide dependancies to services by using dependancy injection without calling constructors
+    // TestBed.configureTestingModule({
+    //   providers: [CalcService, SharedService]
+    // })
+    // shared = TestBed.inject(SharedService)
+    // calc = TestBed.inject(CalcService)
+
+    // If we want to do spyOn() we can do as below
+    shared = jasmine.createSpyObj("SharedService", ["sharedFunction"]) // Instead of create new object here we created Mock Service
+    TestBed.configureTestingModule({
+      providers: [CalcService, {
+        provide: SharedService, useValue: shared
+      }]
+    })
+    shared = TestBed.inject(SharedService)
+    calc = TestBed.inject(CalcService)
+
   })
 
   it("Should multiply two numbers", () => {
@@ -26,7 +45,10 @@ describe("CalcService", () => {
     expect(result).toBe(8)
   })
 
-
+  it("Should call SharedService()", () => {
+    const result = calc.multiply(3, 5)
+    expect(shared.sharedFunction).toHaveBeenCalled()
+  })
 
   // If we want to test multiply and add functions we have to write two tests cases
   // But i here we have repeated codes
